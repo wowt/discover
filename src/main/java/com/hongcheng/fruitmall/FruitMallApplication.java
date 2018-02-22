94ffaed570cde7f1c61b3e6064fb6dc4d4d23648
wanghongcheng <hongchengwang@dealmoon.com>
2018-02-22 10:39:04 +0800

    调通数据库连接配置

--- src/main/java/com/hongcheng/fruitmall/FruitMallApplication.java
+++ src/main/java/com/hongcheng/fruitmall/FruitMallApplication.java
@@ -1,7 +1,9 @@
 package com.hongcheng.fruitmall;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
@@ -9,7 +11,8 @@ import java.util.TimeZone;
 
 @ComponentScan("com.hongcheng.fruitmall")
 @ImportResource("classpath:applicationContext.xml")
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class FruitMallApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/hongcheng/fruitmall/FruitMallApplicationTests.java
+++ src/test/java/com/hongcheng/fruitmall/FruitMallApplicationTests.java
@@ -2,24 +2,27 @@ package com.hongcheng.fruitmall;
 
 import com.hongcheng.fruitmall.mall.dao.mapper.CartEntityMapper;
 import com.hongcheng.fruitmall.mall.entity.CartEntity;
+import com.hongcheng.fruitmall.mall.service.CartService;
+import org.assertj.core.util.Compatibility;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = FruitMallApplication.class)
 public class FruitMallApplicationTests {
 
-	@Resource
-	private CartEntityMapper mapper;
+	@Autowired
+	private CartService service;
 
 	@Test
 	public void test() {
-		CartEntity entity = mapper.getById(1);
-		System.out.println(entity);
+		service.getById();
+
 	}
 
 }
